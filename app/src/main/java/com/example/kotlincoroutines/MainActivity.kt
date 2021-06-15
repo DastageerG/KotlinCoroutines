7ab0137f7fcac4d5596094cc46924ee32c5fa6a9
Dastageerg <dastageerg44@gmail.com>
2021-06-15 02:59:15 -0700

    exercise 1

--- app/src/main/java/com/example/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/example/kotlincoroutines/MainActivity.kt
@@ -2,40 +2,58 @@ package com.example.kotlincoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.lifecycleScope
 import com.example.kotlincoroutines.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity()
 {
-    val  TAG = "1234"
+    val TAG = "1234"
 
-    lateinit var binding:ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        lifecycleScope.launch(Dispatchers.Main)
+
+        binding.buttonGet.isEnabled = false
+        binding.editText.addTextChangedListener(object :TextWatcher
         {
-            delay(3000)
-            binding.textViewMain.text = executeTask().toString()
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int)
+            {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int)
+            {
+                binding.buttonGet.isEnabled = !s.isNullOrEmpty()
+            }
+
+            override fun afterTextChanged(s: Editable?)
+            {
+            } }
+        ) //
+
+        binding.buttonGet.setOnClickListener()
+        {
+            binding.buttonGet.isEnabled = false
+
+            getReputationFromUser(binding.editText.text.toString())
+            binding.editText.text.clear()
+            binding.buttonGet.isEnabled =true
         }
 
     }
-
-    private suspend fun executeTask() : Int
+        fun getReputationFromUser(toString: String)
     {
-       return withContext(Dispatchers.Default)
-       {
-           var a:Int = 0
-           for(i in 1..10)
-           {
-               a++
-           }
-           a
-       }
+        val  reputation =  GetReputationEndpoint().getReputation(toString)
+        Toast.makeText(this,""+reputation,Toast.LENGTH_SHORT).show()
     }
 }
+
